0937809f601aeb7396d523b8e3180ecbccc9b0b2
Malar Saravanan <malarsaravanan1996@gmail.com>
2016-05-22 01:03:43 +0530

    Add files via upload

---
+++ palindromeNUM.c
@@ -0,0 +1,23 @@
+//program for checking prime num or not
+#include<stdio.h>//header file include (preprocessor directives)
+#include<stdlib.h>
+#include<math.h>
+int main()
+{
+    int n,n1,i,r,s=0;
+    printf("Enter the number");
+    scanf("%d",&n);
+    n1=n;
+    while(n>0)
+    {
+    r=n%10;
+    s=s*10+r;
+    n=n/10;
+    }
+    if(s==n1)
+        printf("Palindrome");
+    else
+        printf("Not a Palindrome");
+    return 0;
+
+}
