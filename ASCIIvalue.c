7894969bfa1a0a861b627e49e328e5e15f949727
Malar Saravanan <malarsaravanan1996@gmail.com>
2016-05-22 14:08:02 +0530

    Add files via upload

---
+++ ASCIIvalue.c
@@ -0,0 +1,10 @@
+//program to find the ascii value
+#include<stdio.h>
+#include<stdlib.h>
+int main()
+{ char c;
+ printf("Enter the character");
+ scanf("%c",&c);
+ printf("The ASCII VALUE IS %d",c);
+ return 0;
+ }
